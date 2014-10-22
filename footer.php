ebc7736a387fdd8f5e5a56438424bbfbae123342
Graham F. Scott <gfscott@gmail.com>
2014-10-22 17:58:59 -0400

    fixing homepage masonry to accommodate embedded tweets

--- footer.php
+++ footer.php
@@ -3,13 +3,14 @@
 	<?php $template = get_bloginfo('template_url'); /* Cache the url for the template to use for all the js calls. */ ?>
 	
 	<script>
-		head.js({ jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js'},
-						'<?php echo $template; ?>/js/scripts.js',
-						{fitvids: '<?php echo $template; ?>/js/fitvids.js'},
-						'//api.tiles.mapbox.com/mapbox.js/v1.5.1/mapbox.js',
-						'//platform.twitter.com/widgets.js',
-						'//connect.facebook.net/en_US/all.js#xfbml=1&appId=220399704646080'
-						);
+		head.js(
+		  { jquery:   '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js' },
+		  { main:     '<?php echo $template; ?>/js/scripts.js' },
+			{ fitvids:  '//cdnjs.cloudflare.com/ajax/libs/fitvids/1.1.0/jquery.fitvids.min.js' },
+			{ mapbox:   '//api.tiles.mapbox.com/mapbox.js/v1.5.1/mapbox.js' },
+			{ twitter:  '//platform.twitter.com/widgets.js' },
+			{ facebook: '//connect.facebook.net/en_US/all.js#xfbml=1&appId=220399704646080' }
+		);
 	</script>
 	<!-- Google Analytics	 -->
 	<script>
